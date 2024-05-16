7c559cb77f6a554b20434d98fd08a8184e6531ce
miriameisenhofer <miriam.eisenhofer@rwth-aachen.de>
2024-05-16 22:34:32 +0200

    navigating to second fragment and sending/dealing with count

--- app/src/main/java/com/example/mtgcardscanner/FirstFragment.kt
+++ app/src/main/java/com/example/mtgcardscanner/FirstFragment.kt
@@ -36,7 +36,10 @@ class FirstFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.randomButton.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            val showCountTextView = view.findViewById<TextView>(R.id.textview_first)
+            val currentCount = showCountTextView.text.toString().toInt()
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment(currentCount)
+            findNavController().navigate(action)
         }
 
         // find the toast_button by its ID and set a click listener
