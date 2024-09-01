59705cf3e5c881a2039f03ca6c383bf6280ce61e
Miriam Eisenhofer <miriam.eisenhofer@rwth-aachen.de>
2024-09-01 22:49:15 +0200

    camera

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,11 @@ android {
     namespace = "com.example.mtgcardscanner"
     compileSdk = 34
 
+    buildFeatures{
+        //dataBinding = true
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.mtgcardscanner"
         minSdk = 24
@@ -41,15 +46,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     val cameraxVersion = "1.3.4"
     implementation("androidx.camera:camera-core:${cameraxVersion}")
@@ -57,7 +62,10 @@ dependencies {
     implementation("androidx.camera:camera-lifecycle:${cameraxVersion}")
     implementation("androidx.camera:camera-video:${cameraxVersion}")
 
-    implementation("androidx.camera:camera-view:${cameraxVersion}")
+    //CameraX
+    implementation("androidx.camera:camera-camera2:1.3.4")
+    implementation("androidx.camera:camera-lifecycle:1.3.4")
+    implementation("androidx.camera:camera-view:1.3.4")
     implementation("androidx.camera:camera-extensions:${cameraxVersion}")
     // To recognize Latin script
     implementation("com.google.mlkit:text-recognition:16.0.1")
--- app/src/main/java/com/example/mtgcardscanner/MainActivity.kt
+++ app/src/main/java/com/example/mtgcardscanner/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -16,7 +17,10 @@ import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
 import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
 import com.example.mtgcardscanner.databinding.ActivityMainBinding
 import com.google.mlkit.vision.common.InputImage
@@ -30,7 +34,7 @@ import java.util.concurrent.Executors
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var viewBinding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     private var imageCapture: ImageCapture? = null
 
@@ -39,19 +43,25 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        viewBinding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Request camera permissions
+        Toast.makeText(baseContext,
+            "Requesting permissions...",
+            Toast.LENGTH_SHORT).show()
         if (allPermissionsGranted()) {
+            Toast.makeText(baseContext,
+                "Permissions granted. Starting Camera.",
+                Toast.LENGTH_SHORT).show()
             startCamera()
         } else {
             requestPermissions()
         }
 
         // Set up listeners for take photo and card collection buttons
-        viewBinding.imageCaptureButton.setOnClickListener { takePhoto() }
-        viewBinding.cardCollectionButton.setOnClickListener { viewCardCollection() }
+        binding.btnTakePhoto.setOnClickListener { takePhoto() }
+        binding.cardCollectionButton.setOnClickListener { viewCardCollection() }
 
         cameraExecutor = Executors.newSingleThreadExecutor()
     }
@@ -65,7 +75,41 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle of owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            //Preview
+            Toast.makeText(baseContext,
+                "Preview...",
+                Toast.LENGTH_SHORT).show()
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+            // Select back camera as default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            Toast.makeText(baseContext,
+                "Back camera selected.",
+                Toast.LENGTH_SHORT).show()
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+                Toast.makeText(baseContext,
+                    "Unbound: cameraProvider",
+                    Toast.LENGTH_SHORT).show()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+        }, ContextCompat.getMainExecutor(this))
     }
 
     private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
@@ -104,6 +148,9 @@ class MainActivity : AppCompatActivity() {
                     "Permission request denied",
                     Toast.LENGTH_SHORT).show()
             } else {
+                Toast.makeText(baseContext,
+                    "Permission granted",
+                    Toast.LENGTH_SHORT).show()
                 startCamera()
             }
         }
